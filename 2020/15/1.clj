bfe142f53d190d9f76522f3f226ea72de5c9a31b
Philip Dexter <philip.dexter@gmail.com>
2020-12-15 07:32:38 +0200

    2020 day 15

---
+++ 2020/15/1.clj
@@ -0,0 +1,17 @@
+
+(load-file "../common/common.clj")
+
+(def nums (vec (map to-int (str/split (get input 0) #","))))
+
+(def said (into {} (map-indexed (fn [a b] [b a]) nums)))
+
+(loop [i (count nums)
+       prev (last nums)
+       said said]
+  (if (= i 2020)
+    prev
+    (let [] (if (contains? said prev)
+       (let [prev-last-spoken (get said prev)
+             now (- (dec i) prev-last-spoken)]
+         (recur (inc i) now (assoc said prev (dec i))))
+       (recur (inc i) 0 (assoc said prev (dec i)))))))
