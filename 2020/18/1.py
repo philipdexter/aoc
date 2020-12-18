bc20977a6f265130756d8bc7862c49197caa629d
Philip Dexter <philip.dexter@gmail.com>
2020-12-18 07:43:23 +0200

    2020 day 18

---
+++ 2020/18/1.py
@@ -0,0 +1,19 @@
+import re
+
+class F:
+  def __init__(self, n):
+    self.n = n
+
+  def __add__(self, other):
+    return F(self.n + other.n)
+  def __sub__(self, other):
+    return F(self.n * other.n)
+
+input = []
+for line in open('1.txt'):
+  input.append(re.sub(r'\*', '-', re.sub(r'(\d+)', r'F(\1)', line.strip())))
+
+agg = 0
+for line in input:
+  agg += eval(line).n
+print(agg)
