cdebb04c4824e71c8d5622770ca4dd3a2ea45133
Philip Dexter <philip.dexter@gmail.com>
2023-07-12 21:50:37 +0300

    2022 day 5

---
+++ 2022/5/1.py
@@ -0,0 +1,24 @@
+
+with open('input.txt') as f:
+  lines = f.read().strip().split('\n')
+
+stacks = [
+  'NVCS',
+  'SNHJMZ',
+  'DNJGTCM',
+  'MRWJFDT',
+  'HFP',
+  'JHZTC',
+  'ZLSFQRPD',
+  'WPFDHLSC',
+  'ZGNFPMSD',
+]
+
+for line in lines:
+  move, from_, to = [int(x) for x in line.split(',')]
+  from_ -= 1
+  to -= 1
+  moving = stacks[from_][:move]
+  stacks[from_] = stacks[from_][move:]
+  stacks[to] = moving[::-1] + stacks[to]
+print(''.join([x[0] for x in stacks]))
